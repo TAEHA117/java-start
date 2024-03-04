7f150ab1914a7689739a6e11a56be908516f2ca1
TAEHA117 <nn_goods@naver.com>
2024-03-04 16:22:50 +0900

    문제풀이 : for

---
+++ src/loop/ForEx1.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class ForEx1 {
+    public static void main(String[] args) {
+        /* 문제 : 자연수 출력
+         * 처음 10개의 자연수를 출력하는 프로그램을 작성해 보시오
+         * 이때, count라는 변수를 사용하시오
+         * while, for 2가지 버전의 정답을 만드시오 */
+
+
+        for (int count=1; count <= 10; count++) {
+            System.out.println("자연수 출력 : " + count);
+        }
+    }
+}
